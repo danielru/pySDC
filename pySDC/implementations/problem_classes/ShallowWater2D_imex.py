d9cc47f077b313f8a8085ad5a366e2b11a1ad687
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2017-08-14 15:05:47 +0100

    add template for Gusto/firedrake shallow water problem class

---
+++ pySDC/implementations/problem_classes/ShallowWater2D_imex.py
@@ -0,0 +1,121 @@
+import numpy as np
+from pySDC.core.Problem import ptype
+from pySDC.core.Errors import ParameterError
+
+from firedrake import *
+from gusto import *
+
+# noinspection PyUnusedLocal
+class shallowwater_imex(ptype):
+    """
+    Example implementing the one-dimensional IMEX acoustic-advection
+
+    Attributes:
+        mesh (numpy.ndarray): 1d mesh
+
+    """
+
+    def __init__(self, problem_params, dtype_u, dtype_f):
+        """
+        Initialization routine
+
+        Args:
+            problem_params (dict): custom parameters for the example
+            dtype_u: mesh data
+            dtype_f: mesh data with two components
+        """
+
+        # these parameters will be used later, so assert their existence
+        essential_keys = ['nvars', 'cs', 'cadv', 'order_adv', 'waveno']
+        for key in essential_keys:
+            if key not in problem_params:
+                msg = 'need %s to instantiate problem, only got %s' % (key, str(problem_params.keys()))
+                raise ParameterError(msg)
+
+        # invoke super init, passing number of dofs, dtype_u and dtype_f
+        super(shallowwater_imex, self).__init__(problem_params['nvars'], dtype_u, dtype_f, problem_params)
+
+        
+
+    def solve_system(self, rhs, factor, u0, t):
+        """
+        Simple linear solver for (I-dtA)u = rhs
+
+        Args:
+            rhs (dtype_f): right-hand side for the nonlinear system
+            factor (float): abbrev. for the node-to-node stepsize (or any other factor required)
+            u0 (dtype_u): initial guess for the iterative solver (not used here so far)
+            t (float): current time (e.g. for time-dependent BCs)
+
+        Returns:
+            dtype_u: solution as mesh
+        """
+
+        me = self.dtype_u(self.init)
+
+        return me
+
+    def __eval_fexpl(self, u, t):
+        """
+        Helper routine to evaluate the explicit part of the RHS
+
+        Args:
+            u (dtype_u): current values (not used here)
+            t (float): current time
+
+        Returns:
+            explicit part of RHS
+        """
+
+        fexpl = self.dtype_u(self.init)
+
+        return fexpl
+
+    def __eval_fimpl(self, u, t):
+        """
+        Helper routine to evaluate the implicit part of the RHS
+
+        Args:
+            u (dtype_u): current values
+            t (float): current time (not used here)
+
+        Returns:
+            implicit part of RHS
+        """
+
+
+        fimpl = self.dtype_u(self.init, val=0)
+
+
+        return fimpl
+
+    def eval_f(self, u, t):
+        """
+        Routine to evaluate both parts of the RHS
+
+        Args:
+            u (dtype_u): current values
+            t (float): current time
+
+        Returns:
+            dtype_f: the RHS divided into two parts
+        """
+
+        f = self.dtype_f(self.init)
+        f.impl = self.__eval_fimpl(u, t)
+        f.expl = self.__eval_fexpl(u, t)
+        return f
+
+    def u_exact(self, t):
+        """
+        Routine to compute the exact solution at time t
+
+        Args:
+            t (float): current time
+
+        Returns:
+            dtype_u: exact solution
+        """
+
+        me = self.dtype_u(self.init)
+        return me
