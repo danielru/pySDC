06af111c9f1c06bbfaac5899fa945f7c2adfe728
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2017-08-15 16:42:47 +0100

    dump also initial value in HookClass

--- projects/FastWaveSlowWave/HookClass_shallowwater.py
+++ projects/FastWaveSlowWave/HookClass_shallowwater.py
@@ -8,6 +8,11 @@ class dump_solution(hooks):
       super(dump_solution, self).__init__()
       self.output = File("shallow_water.pvd")
 
+    def pre_run(self, step, level_number):
+      super(dump_solution, self).post_step(step, level_number)
+      L = step.levels[level_number]
+      self.output.write(L.u[0].f)
+
     def post_step(self, step, level_number):
       super(dump_solution, self).post_step(step, level_number)
       L = step.levels[level_number]
