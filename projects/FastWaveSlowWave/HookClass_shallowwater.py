fe1e2687c0c3fdd1d76167caee28c31bd02b55be
Daniel Ruprecht <d.ruprecht@leeds.ac.uk>
2017-08-15 16:35:25 +0100

    add HookClass for shallow water to dump data

---
+++ projects/FastWaveSlowWave/HookClass_shallowwater.py
@@ -0,0 +1,14 @@
+from __future__ import division
+from pySDC.core.Hooks import hooks
+from firedrake import File
+
+class dump_solution(hooks):
+
+    def __init__(self):
+      super(dump_solution, self).__init__()
+      self.output = File("shallow_water.pvd")
+
+    def post_step(self, step, level_number):
+      super(dump_solution, self).post_step(step, level_number)
+      L = step.levels[level_number]
+      self.output.write(L.uend.f)
